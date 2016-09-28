2650fe26b3549d152d0f5a8d219e6a6cd6ef8598
Ben <freakimkaefig87@gmx.de>
2016-09-28 21:37:49 +0200

    First Commit. DwdClient download .txt.gz and decompress it and save it on the local path

---
+++ FspA_Server/DwdClient.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+
+
+namespace FspA_Server
+{
+    class DwdClient
+    { 
+        private int a;
+        private int b;
+        private string adressFtp;
+        private string localPath;
+
+        private FtpWebRequest request;
+        private FtpWebResponse response;
+        private Stream responseStream;
+        private FileStream saveStream;
+
+        public DwdClient()
+        {
+            this.adressFtp = "ftp://ftp-cdc.dwd.de/pub/CDC/derived_germany/soil/daily/recent/derived_germany_soil_daily_recent_232.txt.gz";
+            this.localPath = @"C:\Users\Ben\Documents\Studium Elektrotechnik\S5_Fachstudienprojekt_A\Testentpackt.txt";
+        }
+        ~DwdClient()
+        {
+            this.saveStream.Dispose();
+            this.response.Close();
+            this.responseStream.Close();
+            //File.Delete(this.localPath);
+        }
+
+        public void setAdressFtp(string adressFtp)
+        {
+            this.adressFtp = adressFtp;
+        }
+
+        public string getAdressFtp()
+        {
+            return this.adressFtp;
+        }
+
+        public void setLocalPath(string localPath)
+        {
+            this.localPath = localPath;
+        }
+
+        public string getLocalPath()
+        {
+            return this.localPath;
+        }
+
+        public void connectToFtp()
+        {
+            this.request = (FtpWebRequest)WebRequest.Create(adressFtp);
+            this.request.Method = WebRequestMethods.Ftp.DownloadFile;
+            //Use anonymous logon
+            this.request.Credentials = new NetworkCredential("anonymous", "anonymous");
+        }
+
+        public void getResponseFtp()
+        {
+            this.response = (FtpWebResponse)request.GetResponse();
+
+            Console.WriteLine("Download Complete, status {0}", response.StatusDescription);
+
+            this.responseStream = this.response.GetResponseStream();
+        }
+
+        public void decompressAndSave()
+        {
+            //Erzeugen einer neuen Datei im Pfad: localPath
+            this.saveStream = new FileStream(this.localPath, FileMode.Create);
+            GZipStream decompressionStream = new GZipStream(this.responseStream, CompressionMode.Decompress);
+            decompressionStream.CopyTo(saveStream);
+
+            decompressionStream.Close();
+        }
+    }
+}
--- FspA_Server/Program.cs
+++ FspA_Server/Program.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Net;
+
+
 
 namespace FspA_Server
 {
     class Program
     {
+        public string adressFtp;
+        private string localPath;
+
+        public Program()
+        {
+            this.adressFtp = " ";
+            this.localPath = " ";
+        }
+
         static void Main(string[] args)
         {
+            Program prog = new Program();
+            Console.WriteLine("Geben Sie den gewünschten Ftp-Adresse an:");
+            prog.adressFtp = @Console.ReadLine();
+            Console.WriteLine("Geben Sie den gewünschten Speicherpfad an:");
+            prog.localPath = @Console.ReadLine();
+            Console.WriteLine("Ftp: {0}\n Pfad: {1}", prog.adressFtp, prog.localPath);
+            Console.ReadLine();
         }
     }
 }
